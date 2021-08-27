9bdc0f56798945ff388a6b5fca38fcd7f4d92d49
tnseo0625 <jakeake@naver.com>
2021-08-27 19:32:09 +0900

    2차 저장본(오류가있긴한데 뭐가문제인지모르겠음)

--- app/src/main/java/com/mbtl/mansuhighschool/calculator.java
+++ app/src/main/java/com/mbtl/mansuhighschool/calculator.java
@@ -1,27 +1,73 @@
 package com.mbtl.mansuhighschool;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class calculator extends AppCompatActivity
 {
+    Button sum = findViewById(R.id.button10);
+    Button mp = findViewById(R.id.button11);
+    Button sum_value = findViewById(R.id.sum_value);
+
+    TextView value = findViewById(R.id.value);
+
+    EditText put_value = findViewById(R.id.put_value);
+    EditText put_value2 = findViewById(R.id.put_value2);
+    EditText put_value3 = findViewById(R.id.put_value3);
+
+    int value1, value2, value3;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.choisoonjoo);
-        Button calculator = findViewById(R.id.tnseo);
-        calculator.setOnClickListener(new View.OnClickListener()
+        setContentView(R.layout.calculator);
+
+        sum.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view)
             {
-                Intent intent = new Intent(getApplicationContext(), calculator.class);
-                startActivity(intent);
+                if (TextUtils.isEmpty(put_value.getText())){value1 = 0;}
+                else{ value1 = Integer.parseInt(put_value.getText().toString());}
+                if (TextUtils.isEmpty(put_value2.getText())){value2 = 0;}
+                else{value2 = Integer.parseInt(put_value2.getText().toString());}
+                if (TextUtils.isEmpty(put_value3.getText())){value3 = 0;}
+                else{value3= Integer.parseInt(put_value3.getText().toString());}
+
+                int answer = value3*(2*value1 + (value1-1)*value2) / 2;
+                value.setText(answer);
+
+            }
+        });
+        mp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (TextUtils.isEmpty(put_value.getText())){value1 = 0;}
+                else{ value1 = Integer.parseInt(put_value.getText().toString());}
+                if (TextUtils.isEmpty(put_value2.getText())){value2 = 0;}
+                else{value2 = Integer.parseInt(put_value2.getText().toString());}
+                if (TextUtils.isEmpty(put_value3.getText())){value3 = 0;}
+                else{value3= Integer.parseInt(put_value3.getText().toString());}
+                int finalanswer = 0;
+                if (value2 < 1 && value2 >-1)
+                {
+                    double answer = value1*(1-Math.pow(value2, value3)) / (1-value2);
+                    finalanswer = (int) Math.round(answer);
+                }
+                else
+                {
+                    double answer = value1*(Math.pow(value2, value3)-1) / (value2-1);
+                    finalanswer = (int) Math.round(answer);
+                }
+                value.setText(finalanswer);
             }
         });
     }
