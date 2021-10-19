c6fb7deb8636fb6473e43da62ad13291380718f4
dbdudqls <qlakejakej12@naver.com>
2021-10-20 00:04:31 +0900

    dbdudqls

--- app/src/main/java/com/mbtl/mansuhighschool/yooyoungbin.java
+++ app/src/main/java/com/mbtl/mansuhighschool/yooyoungbin.java
@@ -1,17 +1,23 @@
 package com.mbtl.mansuhighschool;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 public class yooyoungbin extends AppCompatActivity {
 
+    Button upward, backward, right, left;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.yooyoungbin);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);//핸드폰이 다크모드여도 앱 배경 안변하게 함
+
 
     }
 }
