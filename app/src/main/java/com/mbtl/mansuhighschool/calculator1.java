1264fc3815abe3ce805df341be9778d740e100b5
tnseo0625 <jakeake@naver.com>
2021-10-25 00:01:02 +0900

    update(끝난건아님)

--- app/src/main/java/com/mbtl/mansuhighschool/calculator1.java
+++ app/src/main/java/com/mbtl/mansuhighschool/calculator1.java
@@ -1,13 +1,63 @@
 package com.mbtl.mansuhighschool;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class calculator1 extends AppCompatActivity{
+
+    ImageView math1,math2,math3;
+    EditText input;
+    TextView textview3;
+    int a,b,c;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator1);
+
+        math1 = findViewById(R.id.math1);
+        math2 = findViewById(R.id.math2);
+        math3 = findViewById(R.id.math3);
+
+        input = findViewById(R.id.input);
+        textview3 = findViewById(R.id.textView3);
+
+        math1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                a = Integer.parseInt(input.getText().toString());
+                b = a*(a+1)/2;
+
+                textview3.setText(String.valueOf(b));
+            }
+        });
+        math2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                a = Integer.parseInt(input.getText().toString());
+                b = a*(a+1)*(a*2+1)/6;
+
+                textview3.setText(String.valueOf(b));
+            }
+        });
+        math3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                a = Integer.parseInt(input.getText().toString());
+                b = a*(a+1)/2;
+                c = b*b;
+
+                textview3.setText(String.valueOf(c));
+            }
+        });
     }
 }
--- app/src/main/java/com/mbtl/mansuhighschool/calculator2.java
+++ app/src/main/java/com/mbtl/mansuhighschool/calculator2.java
@@ -1,13 +1,62 @@
 package com.mbtl.mansuhighschool;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class calculator2 extends AppCompatActivity{
+
+    TextView log,answer,jisu;
+    EditText input_value1,input_value2;
+
+    int a,b,c;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calculator2);
+
+        log = findViewById(R.id.log);
+        jisu = findViewById(R.id.jisu);
+        answer = findViewById(R.id.answer);
+        input_value1 = findViewById(R.id.input_value1);
+        input_value2 = findViewById(R.id.input_value2);
+
+
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                a = Integer.parseInt(input_value1.getText().toString());
+                b = Integer.parseInt(input_value2.getText().toString());
+                c = 1;
+                while(a != b)
+                {
+                    c = c+1;
+                    a = a*a;
+                }
+                answer.setText(String.valueOf(c));
+            }
+        });
+        jisu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                a = Integer.parseInt(input_value1.getText().toString());
+                b = Integer.parseInt(input_value2.getText().toString());
+                c = a;
+                while(b != 1)
+                {
+                    a = a*c;
+                    b = b-1;
+                }
+                answer.setText(String.valueOf(a));
+            }
+        });
     }
 }
